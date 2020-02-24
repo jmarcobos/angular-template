d0ddf116ffbf063489eec8220076cb2332b7e98b
Javier Marín Cobos <j.marin@umh.es>
2020-02-24 14:45:48 +0100

    Menu finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,21 @@ export class AppComponent implements OnInit {
   
   public title = 'Universite';
   public buscadorVisible: boolean;
+  public menuVisible: boolean;
 
   constructor() { }
 
   ngOnInit() {
     this.buscadorVisible = false;
+    this.menuVisible = false;
   }
 
   gestionBuscador() {
     this.buscadorVisible = !this.buscadorVisible;
   }
+
+  gestionMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
 }
