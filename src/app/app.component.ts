3ab55a6920091b6a3a4000d591390b944e620d3c
Javier Marín Cobos <j.marin@umh.es>
2020-02-27 11:30:08 +0100

    Applications searcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   public menuVisible: boolean;
   public optionActiva: string;
   public aplicaciones: [];
+  public appFiltradas: any;
   public escritorio: any;
   public totalEscritorio: number = 0;
   public foto: any;
@@ -27,22 +28,30 @@ export class AppComponent implements OnInit {
     this.getDatosInicio();
   }
 
-  gestionBuscador() {
+  gestionBuscador(option: string) {
     this.buscadorVisible = !this.buscadorVisible;
+    if (option) {
+      this.gestionSidenav(option);
+    }
   }
 
   gestionMenu() {
     this.menuVisible = !this.menuVisible;
   }
 
-  gestionSidenav(option) {
+  gestionSidenav(option: string) {
     this.optionActiva = option;
+  }
+
+  buscar(busqueda) {     
+    console.log(busqueda.target.value);
   }
 
   private getDatosInicio() {
     this.inicialService.getDatosInicio()
       .subscribe((response) => {
         this.aplicaciones = response[0];
+        this.appFiltradas = this.aplicaciones;
         this.configuracion = response[1];
         this.escritorio = response [2];
         this.totalEscritorio = this.escritorio.AplicacionesEscritorio.length;
